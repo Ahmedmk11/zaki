a4ca5002dee35902f98106367b94439100d20063
Ahmed Mahmoud <ahmedmahmoudkzm@gmail.com>
2025-07-17 21:32:06 +0300

    Added backend basic configuration

---
+++ backend/src/database/database.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+
+const mongoUri = process.env.MONGO_URI
+if (!mongoUri) {
+    throw new Error('MONGO_URI is not defined in the environment variables')
+}
+
+@Module({
+    imports: [MongooseModule.forRoot(mongoUri)],
+})
+export class DatabaseModule {}
